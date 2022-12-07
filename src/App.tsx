95b93d954d6b49aedf7360832daf3e4eba304f9d
郑一亮 <zheng.yiliang@qq.com>
2022-12-07 21:17:14 +0800

    feat: init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.less';
+
+function App() {
+  // console.log(process.env)
+  // const arr = [1, 2, 3];
+
+  // const b = [...arr];
+
+  // const c = b ?? 'a';
+
+  return <h2>webpack5-react-ts1231223</h2>;
+}
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const root = document.getElementById('root');
+
+if (root) {
+  createRoot(root).render(<App />);
+}
